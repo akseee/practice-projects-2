3ed27038441f36a481fd5d428305590d7a4de7ed
Akseenja <klasik1995@bk.ru>
2024-12-01 17:21:30 +0300

    feat: complete tabs

--- christmas-shop/scripts/gifts.js
+++ christmas-shop/scripts/gifts.js
@@ -1,8 +1,32 @@
-async function loadCards() {
+const tabs = document.querySelectorAll(".gift__controls .gift__controls-item")
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", (e) => {
+    tabs.forEach((element) => {
+      element.classList.remove("active")
+    })
+    e.target.classList.add("active")
+
+    const selectedCategory = e.target.dataset.category
+    loadCards(selectedCategory)
+  })
+})
+
+function sortTabs(list, tab) {
+  if (tab === "all") {
+    return list
+  }
+  return list.filter((card) => {
+    return card.category === tab
+  })
+}
+
+async function loadCards(category = "all") {
   const cards = await fetchData()
   if (cards) {
     const newArr = shuffleArray(cards)
-    giftTemplate(newArr, ".gift__list")
+    const sortedArr = sortTabs(newArr, category)
+    giftTemplate(sortedArr, ".gift__list")
   } else {
     console.error("Something went wrong: no cards data available")
   }
