699d9eddd0c328fc84d3df5bce2aff15cbddd48c
Akseenja <klasik1995@bk.ru>
2025-01-16 02:55:04 +0300

    feat: disabled buttons on difficulty change

--- simon-says/src/scripts/AppPresenter.js
+++ simon-says/src/scripts/AppPresenter.js
@@ -118,6 +118,7 @@ export default class AppPresenter {
 		this.keyboard.setupKeyboard(this.model.keys)
 
 		this.view.setActiveDifficultyButton(difficulty)
+		this.keyboard.disableKeyboard()
 	}
 
 	bindButtonListeners() {
