cbd68120d67675eea14548ffdee1f0c830aadfc1
Akseenja <klasik1995@bk.ru>
2025-02-22 11:32:55 +0300

    feat: migrate news.js to news.ts with typization

--- news-api/src/components/view/news/news.ts
+++ news-api/src/components/view/news/news.ts
@@ -1,20 +1,32 @@
 import './news.css';
 
+interface INewsItem {
+    urlToImage: URL;
+    author?: string;
+    source: {
+        name: string;
+    };
+    publishedAt: string;
+    title: string;
+    description: string;
+    url: string;
+}
+
 class News {
-    draw(data) {
+    draw(data: INewsItem[]) {
         const news = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
 
         const fragment = document.createDocumentFragment();
-        const newsItemTemp = document.querySelector('#newsItemTemp');
+        const newsItemTemp = document.querySelector('#newsItemTemp') as HTMLTemplateElement | null;
 
-        news.forEach((item, idx) => {
-            const newsClone = newsItemTemp.content.cloneNode(true);
+        news.forEach((item: INewsItem, idx: number) => {
+            const newsClone = newsItemTemp.content.cloneNode(true) as DocumentFragment;
 
             if (idx % 2) newsClone.querySelector('.news__item').classList.add('alt');
+            const metaPhoto = newsClone.querySelector('.news__meta-photo') as HTMLDivElement | null;
 
-            newsClone.querySelector('.news__meta-photo').style.backgroundImage = `url(${
-                item.urlToImage || 'img/news_placeholder.jpg'
-            })`;
+            metaPhoto.style.backgroundImage = `url(${item.urlToImage || 'img/news_placeholder.jpg'})`;
+
             newsClone.querySelector('.news__meta-author').textContent = item.author || item.source.name;
             newsClone.querySelector('.news__meta-date').textContent = item.publishedAt
                 .slice(0, 10)
