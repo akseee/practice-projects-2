8f0a6b4022bc5e9dd38c4e8a5309713eff8f3cce
Akseenja <klasik1995@bk.ru>
2025-02-22 13:00:02 +0300

    feat: migrate app.js to .ts

---
+++ news-api/src/components/app/app.ts
@@ -0,0 +1,24 @@
+import { INewsResponse, ISourcesResponse } from '../../types';
+import AppController from '../controller/controller';
+import { AppView } from '../view/appView';
+
+class App {
+    private controller: AppController;
+    private view: AppView;
+
+    constructor() {
+        this.controller = new AppController();
+        this.view = new AppView();
+    }
+
+    start(): void {
+        document
+            .querySelector('.sources')
+            .addEventListener('click', (e: MouseEvent) =>
+                this.controller.getNews(e, (data: INewsResponse) => this.view.drawNews(data))
+            );
+        this.controller.getSources((data: ISourcesResponse) => this.view.drawSources(data));
+    }
+}
+
+export default App;
