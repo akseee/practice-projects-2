6f52a46119043de39abbd889c4d5bc5a300012cd
Akseenja <klasik1995@bk.ru>
2025-03-15 14:19:34 +0300

    refactor: change add and change views

--- decision-making-tool/src/components/view/footer/footer-view.ts
+++ decision-making-tool/src/components/view/footer/footer-view.ts
@@ -1,8 +1,10 @@
+import BaseComponent from "../../common/base-component"
 import { View } from "../view"
 
 const enum CSSClasses {
   WRAPPER = "wrapper",
   FOOTER = "footer",
+  LINK = "link",
 }
 
 export default class FooterView extends View {
@@ -12,5 +14,14 @@ export default class FooterView extends View {
       classNames: [CSSClasses.WRAPPER, CSSClasses.FOOTER],
     }
     super(parameters)
+    this.setLink("gh@akseee, 2025")
+  }
+
+  public setLink(text: string): void {
+    this.destroyChildren()
+    const link = new BaseComponent({ tag: "a", classNames: [CSSClasses.LINK] })
+    link.setTextContent(text)
+    link.setAttribute("href", "https://github.com/akseee")
+    this.appendChildComponent(link)
   }
 }
--- decision-making-tool/src/components/view/main/main-view.ts
+++ decision-making-tool/src/components/view/main/main-view.ts
@@ -1,3 +1,4 @@
+import type BaseComponent from "../../common/base-component"
 import { View } from "../view"
 
 const enum CSSClasses {
@@ -6,11 +7,17 @@ const enum CSSClasses {
 }
 
 export default class MainView extends View {
-  constructor() {
+  constructor(content: BaseComponent) {
     const parameters = {
       tag: "main",
       classNames: [CSSClasses.WRAPPER, CSSClasses.MAIN],
     }
     super(parameters)
+    this.setContent(content)
+  }
+
+  public setContent(component: HTMLElement | BaseComponent): void {
+    this.destroyChildren()
+    this.appendChildComponent(component)
   }
 }
