8da93bd7835464087c0529309b5bc211cb074f18
Akseenja <klasik1995@bk.ru>
2025-04-29 20:03:00 +0300

    feat: add mock for new messages

--- fun-chat/src/components/user-item/user-item.ts
+++ fun-chat/src/components/user-item/user-item.ts
@@ -3,17 +3,28 @@ import BaseComponent from "../../shared/view/base-component"
 
 export default class UserItem extends BaseComponent {
   public wrapper: BaseComponent
-
+  public messagesAmount: BaseComponent
+  public newMessages: boolean
   constructor() {
     super({ tag: "li", classNames: ["chat__user-item"] })
 
     this.wrapper = new BaseComponent({ tag: "a", classNames: ["chat__user-item-wrapper"] })
 
     this.appendChildComponent(this.wrapper)
+    this.newMessages = false
+    this.messagesAmount = new BaseComponent({
+      tag: "div",
+      classNames: ["chat__user-item__messages-amount"],
+    })
   }
 
   public setUserIItemData(user: TUser): void {
     this.setLogin(user.login)
+
+    if (user.login === "123" || user.login == "432") {
+      this.setNewMessages(1235)
+    }
+
     this.setStatus(user.isLogined)
     this.setAttribute("dataset-login", user.login)
   }
@@ -44,5 +55,24 @@ export default class UserItem extends BaseComponent {
     } else {
       this.wrapper.removeClass("active")
     }
+  }
+
+  public setNewMessages(amount: number = 0): void {
+    const text = new BaseComponent({
+      tag: "p",
+      classNames: ["chat__user-item__messages-amount__text"],
+    })
+
+    if (amount > 0) {
+      this.newMessages = true
+      text.setTextContent(`${amount}`)
+      this.messagesAmount.show()
+    } else {
+      this.newMessages = false
+      this.messagesAmount.hide()
+    }
+
+    this.messagesAmount.appendChildComponent(text)
+    this.wrapper.appendChildComponent(this.messagesAmount)
   }
 }
